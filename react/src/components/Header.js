6c9d88cfac65371224b1c73bc308603effdbb96c
julianflancheros <jflancherosm@unal.edu.co>
2023-04-09 16:23:00 -0500

    Create new div containers to split the information and load the logo image

--- react/src/components/Header.js
+++ react/src/components/Header.js
@@ -1,12 +1,25 @@
 import React from 'react';
 
-import '../header.css'
+import '../header.css'; 
+
+import logo from "../images/Logo.png" ; 
 export class Header extends React.Component {
     render() {
         return (
-            <header>
-                <h1>CAUDAL AMBIENTAL</h1>
-                <p className="subtitle-note">Herramienta para el calculo de caudal ambiental y porcentaje de aprovechamiento en cualquier punto de una cuenca</p>
+            <header className='header-section'>
+                <div className='header-section_name'>
+                    <h1>HeCCA</h1>
+                    <small>Versión 2.0</small>
+                </div>
+                
+                <div>
+                    <p className='subtitle-note'>Herramienta para el calculo de Caudal Ambiental </p>
+                </div>
+
+                <div className='header-section_brand'>
+                    <p>Un proyecto de </p>
+                    <img src={logo} alt='Logo Semillero SIHU'/>
+                </div>
             </header>
         )
     }
